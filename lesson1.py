918c649e7e967b2754b7a6e0e5796515fa10e2fd
Paul Sekole <sekole2017@gmail.com>
2026-07-14 02:34:59 +0200

    completed lesson1

---
+++ lesson1.py
@@ -0,0 +1,29 @@
+print("Hello Paul")
+name = "Paul Sekole"
+age = 27
+university = "University of Limpopo"
+degree = "Computer Science and Mathematics"
+career = "Data Sciences"
+print(name)
+print(age)
+print(university)
+print(degree)
+print(career)
+
+name = "Mohlatlego"
+career = "Software engineer"
+print(name)
+print(career)
+
+name = "Paul"
+career = "Data Scientist"
+print(name, "wants to become a", career)
+
+team = "Kaizer Chiefs"
+programming = "python"
+food = "pap and beef"
+number = "6"
+print(team)
+print(programming)
+print(food)
+print(number)
--- test.py
+++ test.py
@@ -6,3 +6,27 @@ print(name)
 print(university)
 print(degree)
 print(career)
+name = "Paul"
+age = 27
+print(type(name))
+print(type(age))
+name = "Paul"
+age = 27
+height = 7.01
+graduated = True
+print(name)
+print(age)
+print(height)
+print(graduated)
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(graduated))
+programming = "python"
+team = "Kaizer Chiefs"
+number = 6
+mathematics = True
+print(type(programming))
+print(type(team))
+print(type(number))
+print(type(mathematics))

0bf795758ffd9d6626c8c4c6135667544a953eb4
Paul Sekole <sekole2017@gmail.com>
2026-07-15 17:26:29 +0200

    completed lesson3-input  & type conversion

---
+++ lesson3.py
@@ -0,0 +1,7 @@
+
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+programming = input("What is your favorite programming language? ")
+print("Hello", name)
+print("Next year you will be", age +1 )
+print("Your favorite programming language is", programming)

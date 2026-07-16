d4fcd9ae584cbdcacda629befcd9b4c8d4f377df
Paul Sekole <sekole2017@gmail.com>
2026-07-16 18:22:54 +0200

    Completed lesson4-Aritmetic operators

---
+++ lesson4.py
@@ -0,0 +1,86 @@
+code1 = 250
+code2 = 125
+output = code1-code2
+
+print("Difference = ", output)
+
+num1 = 700
+num2 = 1000
+results = num1-num2
+
+print("Subtraction =", results)
+
+num3 = 745
+num4 = 9
+results = num3*num4
+
+print("Multiplication =", results)
+
+num5 = 150
+num6 = 75
+results = num5/num6
+
+print("Division =", results)
+
+num7 = 100
+num8 = 6
+results = num7 // num8
+
+print("floor division =", results)
+
+num9 = 350
+num10 = 10
+results = num9 % num10
+
+print("Remainder =", results)
+
+num11 = 5
+results = num11**2
+
+print("Exponent =", results)
+
+print("---------Arithmetic Proj-----------")
+
+num1 = 50
+num2 = 7
+output = num1 + num2
+
+print("Addition =", output)
+
+num1 = 50
+num2 = 7
+results = num1 - num2
+
+print("Subtraction =", results)
+
+num1 = 50
+num2 = 7
+results = num1 * num2
+
+print("Multiplication =", results)
+
+num1 = 50
+num2 = 7
+results = num1 / num2
+
+print("Division =", results)
+
+num1 = 50
+num2 = 7
+results = num1 // num2
+
+print("Floor Division =", results)
+
+num1 = 50
+numb2 = 7
+results = num1 % num2
+
+print("Remainder =", results)
+
+num1 = 50
+num2 = 7
+results = num1 ** num2
+
+print("Power =", results)
+
+print("---------------------------------------")

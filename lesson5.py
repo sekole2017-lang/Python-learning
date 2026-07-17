5527b5829bd91325216d13746126d361d5a4d37b
Paul Sekole <sekole2017@gmail.com>
2026-07-17 16:51:25 +0200

    Lesson5 completed-comparison operators

---
+++ lesson5.py
@@ -0,0 +1,34 @@
+num1  = 10
+num2 = 10
+
+print(num1==num2)
+
+num1 = 10
+num2 = 5
+
+print(num1 != num2)
+
+num1 = 10
+num2 = 5
+print(num1 > num2)
+
+num1 = 15
+num2 = 5
+
+print(num2 < num1)
+
+print("-----------Comparison operators--------------")
+
+age = 27
+pass_mark = 60
+score = 68
+
+print("Age is greater than 18:", age > 18)
+print("pass mark is less than pass score:", pass_mark < score)
+print("Age is less or equal to 30:", age <= 30)
+print("score pass is below pass mark:",score < pass_mark)
+print("Age is equal to 39:", age == 39)
+print("Pass mark is not equal to score:", pass_mark != score)
+print("Age is greater or equal to 29:", age >= 29)
+
+print("-------------------------------------------")

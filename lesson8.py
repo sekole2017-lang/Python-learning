4d13ae1129e68c69c7b5b4afb4f63899479cbf59
Paul Sekole <sekole2017@gmail.com>
2026-07-23 00:28:38 +0200

    Completed lesson8-While & for Loops

---
+++ lesson8.py
@@ -0,0 +1,47 @@
+count = 1
+
+while count <= 5:
+    print(count)
+    count = count + 1
+
+password = ""
+while password != "Paul@26":
+ password = input("Enter your password: ")
+print("Access granted")
+
+count = 1
+while count <= 3:
+   print("Hello")
+   count = count + 1
+
+
+print("Finished")
+
+print("---------for loops----------------------------")
+
+for number in range(5):
+   print(number)
+
+
+print("---------------------------------------")
+
+for number in range(1,6):
+   print(number)
+
+for name in range(3):
+   print("Paul")
+
+
+for number in range(5,0,-1):
+   print(number)
+
+print("Halt")
+
+for number in range(10,60,+10):
+   print(number)
+
+for surname in range(5):
+   print("Sekole")
+
+for number in range(2,22,+2):
+   print(number)
